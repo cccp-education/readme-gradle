206e520926e9676330563b1894f0c4c96fa7da2d
cheroliv <cheroliv@users.noreply.github.com>
2026-06-11 19:53:39 +0200

    Session 006 — Publication Maven Central 0.0.1 (nmcp, codebase upgrade, annotations fix) + cloture

--- readme-plugin/settings.gradle.kts
+++ readme-plugin/settings.gradle.kts
@@ -1,5 +1,36 @@
-pluginManagement.repositories {
-    mavenLocal()
-    gradlePluginPortal()
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
+    id("com.gradleup.nmcp.settings").version("1.5.0")
+}
+
+val globalProps = java.util.Properties().also {
+    val globalFile = file(System.getProperty("user.home") + "/.gradle/gradle.properties")
+    if (globalFile.exists()) it.load(globalFile.inputStream())
+}
+
+nmcpSettings {
+    centralPortal {
+        username = globalProps.getProperty("ossrhUsername") ?: error("ossrhUsername not found")
+        password = globalProps.getProperty("ossrhPassword") ?: error("ossrhPassword not found")
+        publishingType = "AUTOMATIC"
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
 rootProject.name = "readme-plugin"
